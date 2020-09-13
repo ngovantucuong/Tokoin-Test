e9fa0417bb76f073faa59401d168f7bf151126b5
Ngo Van Tu Cuong <cuongnvt@vnext.com.vn>
2020-09-13 22:30:14 +0700

    - Add test case

--- Tokoin-Test/Tokoin-TestTests/Tokoin_TestTests.swift
+++ Tokoin-Test/Tokoin-TestTests/Tokoin_TestTests.swift
@@ -11,8 +11,10 @@ import XCTest
 
 class Tokoin_TestTests: XCTestCase {
 
+    var sut: URLSession!
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = URLSession(configuration: .default)
     }
 
     override func tearDown() {
@@ -30,5 +32,22 @@ class Tokoin_TestTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    func testApiData() {
+      let url = URL(string: "http://newsapi.org/v2/everything?q=bitcoin&from=2020-08-13&sortBy=publishedAt&apiKey=98f63962d94140dbb0595d20c495678a")
+      let promise = expectation(description: "Status code: 200")
+      let dataTask = sut.dataTask(with: url!) { data, response, error in
+        if let error = error {
+          XCTFail("Error: \(error.localizedDescription)")
+          return
+        } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+            XCTAssertEqual(statusCode, 200)
+            promise.fulfill()
+        }
+      }
+      dataTask.resume()
+      wait(for: [promise], timeout: 5)
+    }
+
 
 }
