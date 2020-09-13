123f63cf69e4faaba9058ff05705af75260dd29b
Ngo Van Tu Cuong <cuongnvt@vnext.com.vn>
2020-09-13 16:10:10 +0700

    - Init project

---
+++ Tokoin-Test/Tokoin-Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tokoin-Test
+//
+//  Created by ngovantucuong on 9/13/20.
+//  Copyright © 2020 ngovantucuong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
